3049b0fde45f8f7ff3fc2d0a8a3b113e4285d5b3
Steven Clontz <steven.clontz@gmail.com>
2014-01-27 14:20:51 -0600

    Clarify placement of new filters

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,6 +1,10 @@
+# Default nanoc-au Helpers/Filters
 include Nanoc::Helpers::Breadcrumbs
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 
 include OrderItems
 include SidebarMenu
+
+# Add new Helpers/Filters here:
+
